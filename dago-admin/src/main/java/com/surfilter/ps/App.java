c49b881269da237a20c25144d18b509aa719b5ce
Alex Yao <yaohao803@163.com>
2018-05-28 12:21:33 +0800

    update pom

--- dago-admin/src/main/java/com/surfilter/ps/App.java
+++ dago-admin/src/main/java/com/surfilter/ps/App.java
@@ -2,12 +2,25 @@ package com.surfilter.ps;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
     }
 }
