225ea65b37deb248a8f0b9f239b81f10b21e0868
Alex <yaohao@1218.com.cn>
2018-05-28 22:03:08 +0800

    在启动类中增加profile的日志显示

--- dago-admin/src/main/java/com/surfilter/ps/Application.java
+++ dago-admin/src/main/java/com/surfilter/ps/Application.java
@@ -1,14 +1,22 @@
 package com.surfilter.ps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Hello world!
  */
 @SpringBootApplication
 public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
+        for (String profile:activeProfiles) {
+            LOGGER.warn("Spring Boot 使用profiler为:{}",profile);
+        }
     }
 }
