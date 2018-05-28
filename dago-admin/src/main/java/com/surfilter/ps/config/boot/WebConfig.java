b049768ebfe7e99f259bb10f8ddfbeba345bb12e
yaohao803 <yaohao803@163.com>
2018-05-28 15:40:56 +0800

    增加配置文件 Signed-off-by: yaohao803 <yaohao803@163.com>

---
+++ dago-admin/src/main/java/com/surfilter/ps/config/boot/WebConfig.java
@@ -0,0 +1,22 @@
+package com.surfilter.ps.config.boot;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import com.surfilter.ps.config.web.CustomRequestMappingHandlerMapping;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport{
+	
+	@Override
+    @Bean
+    public RequestMappingHandlerMapping requestMappingHandlerMapping() {
+        RequestMappingHandlerMapping handlerMapping = new CustomRequestMappingHandlerMapping();
+        handlerMapping.setOrder(0);
+        handlerMapping.setInterceptors(getInterceptors());
+        return handlerMapping;
+    }
+
+}
