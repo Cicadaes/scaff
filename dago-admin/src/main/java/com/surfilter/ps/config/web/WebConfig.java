7fca35ce7dfc22646eb20b94122d58cbca667785
Alex <yaohao@1218.com.cn>
2018-05-29 09:06:12 +0800

    在启动类中增加profile的日志显示

--- dago-admin/src/main/java/com/surfilter/ps/config/web/WebConfig.java
+++ dago-admin/src/main/java/com/surfilter/ps/config/web/WebConfig.java
@@ -3,6 +3,9 @@ package com.surfilter.ps.config.web;
 import org.apache.catalina.filters.RemoteIpFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
  * All rights Reserved, Designed By www.1218.com.cn
@@ -17,10 +20,20 @@ import org.springframework.context.annotation.Configuration;
  * 注意：本内容仅限于任子行网络技术股份有限公司内部传阅，禁止外泄以及用于其他的商业目
  */
 @Configuration
-public class FilterConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public RemoteIpFilter remoteIpFilter() {
         return new RemoteIpFilter();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        return new LocaleChangeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
