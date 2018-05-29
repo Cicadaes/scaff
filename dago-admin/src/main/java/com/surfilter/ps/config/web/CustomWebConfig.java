da9491862a2e062de9eb805856514dbacebee8c1
yaohao803 <yaohao803@163.com>
2018-05-29 11:18:05 +0800

    修改WebConfig的名称，解决和springboot webconfig bean名称冲突的问题

--- dago-admin/src/main/java/com/surfilter/ps/config/web/CustomWebConfig.java
+++ dago-admin/src/main/java/com/surfilter/ps/config/web/CustomWebConfig.java
@@ -13,14 +13,14 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
  * @version V1.0
  * @Title: WebConfig
  * @Package com.surfilter.ps.config.web
- * @Description:过滤配置类
+ * @Description:自定义web配置类
  * @author: Alex
  * @date: 2018-05-28 下午9:33
  * @Copyright: 2018 www.1218.com.cn Inc. All rights reserved.
  * 注意：本内容仅限于任子行网络技术股份有限公司内部传阅，禁止外泄以及用于其他的商业目
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class CustomWebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
     public RemoteIpFilter remoteIpFilter() {
