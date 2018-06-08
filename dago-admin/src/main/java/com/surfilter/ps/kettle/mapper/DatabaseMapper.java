58d73f97ab23f3bff3610cbf5cd39228b8b112c8
yaohao803 <yaohao803@163.com>
2018-06-08 09:32:27 +0800

    临时备份

--- dago-admin/src/main/java/com/surfilter/ps/kettle/mapper/DatabaseMapper.java
+++ dago-admin/src/main/java/com/surfilter/ps/kettle/mapper/DatabaseMapper.java
@@ -2,7 +2,6 @@ package com.surfilter.ps.kettle.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
@@ -13,7 +12,7 @@ import com.surfilter.ps.kettle.entity.Database;
 
 public interface DatabaseMapper {
 
-	@Results(id = "BaseResultMap", value = { @Result(property = "idDatabase", column = "idDatabase", id = true),
+	@Results(id = "BaseResultMap2", value = { @Result(property = "idDatabase", column = "idDatabase", id = true),
 			@Result(property = "firstName", column = "first_name"),
 			@Result(property = "lastName", column = "last_name") })
 
@@ -25,63 +24,22 @@ public interface DatabaseMapper {
 	 */
 	@Select("select * from r_database where ID_DATABASE = #{id}")
 	Database getById(Integer id);
-	
-	@SelectProvider(type = DatabaseSqlBuilder.class, method = "buildGetUsersByName")
+
+	@SelectProvider(type = DatabaseSqlBuilder.class, method = "getByObject")
 	List<Database> query(Database database);
 
-	@SelectProvider(type = UserSqlBuilder.class, method = "buildGetUsersByName")
-	List<Database> getUsersByName(@Param("name") String name, @Param("orderByColumn") String orderByColumn);
-	
-	
 	class DatabaseSqlBuilder {
 		// If not use @Param, you should be define same arguments with mapper method
-		public static String buildGetUsersByName(final String name, final String orderByColumn) {
+		public static String getByObject(final String name, final Database database) {
 			return new SQL() {
 				{
 					SELECT("*");
 					FROM("users");
 					WHERE("name like #{name} || '%'");
-					ORDER_BY(orderByColumn);
 				}
 			}.toString();
 		}
 
-		// If use @Param, you can define only arguments to be used
-		public static String buildGetUsersByName(@Param("orderByColumn") final String orderByColumn) {
-			return new SQL() {
-				{
-					SELECT("*");
-					FROM("users");
-					WHERE("name like #{name} || '%'");
-					ORDER_BY(orderByColumn);
-				}
-			}.toString();
-		}
 	}
 
-	class UserSqlBuilder {
-		// If not use @Param, you should be define same arguments with mapper method
-		public static String buildGetUsersByName(final String name, final String orderByColumn) {
-			return new SQL() {
-				{
-					SELECT("*");
-					FROM("users");
-					WHERE("name like #{name} || '%'");
-					ORDER_BY(orderByColumn);
-				}
-			}.toString();
-		}
-
-		// If use @Param, you can define only arguments to be used
-		public static String buildGetUsersByName(@Param("orderByColumn") final String orderByColumn) {
-			return new SQL() {
-				{
-					SELECT("*");
-					FROM("users");
-					WHERE("name like #{name} || '%'");
-					ORDER_BY(orderByColumn);
-				}
-			}.toString();
-		}
-	}
 }
---
+++ dago-admin/src/main/java/com/xxl/conf/admin/service/IXxlConfNodeService.java
@@ -0,0 +1,28 @@
+package com.xxl.conf.admin.service;
+
+
+import com.xxl.conf.admin.core.model.XxlConfNode;
+import com.xxl.conf.admin.core.model.XxlConfUser;
+import com.xxl.conf.admin.core.util.ReturnT;
+
+import java.util.Map;
+
+/**
+ * @author xuxueli 2015-9-4 18:19:52
+ */
+public interface IXxlConfNodeService {
+
+	public Map<String,Object> pageList(int offset,
+									   int pagesize,
+									   String env,
+									   String appname,
+									   String key,
+									   XxlConfUser loginUser);
+
+	public ReturnT<String> delete(String env, String key, XxlConfUser loginUser);
+
+	public ReturnT<String> add(XxlConfNode xxlConfNode, XxlConfUser loginUser);
+
+	public ReturnT<String> update(XxlConfNode xxlConfNode, XxlConfUser loginUser);
+
+}
